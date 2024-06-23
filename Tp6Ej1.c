21743aae6f1748968d3d5e761298da07a594b621
InakiFarias <iniakifariasg@gmail.com>
2024-06-23 17:44:04 -0300

    cambios ej 1 tp 6

--- Tp6Ej1.c
+++ Tp6Ej1.c
@@ -1,13 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void inicialiarVector(int vec[], int n);
+
 int main(){
-    int vec[50], acum = 0, prod = 1;
+    int vec[15], acum = 0, prod = 1, cantElem = 15;
 
-    for(int i = 0; i < 15; i++){
-        printf("Ingrese un valor: ");
-        scanf("%d", &vec[i]);
-
+    inicialiarVector(vec[cantElem], cantElem);
+    for(int i = 0; i < cantElem; i++){
         acum += vec[i];
         prod *= vec[i];
     }
@@ -19,4 +19,11 @@ int main(){
         printf("Valor de la posicion %d: %d\n", i + 1, vec[i]);
     }
     return 0;
+}
+
+void inicialiarVector(int vec[], int n){
+    for(int i = 0; i < n; i++){
+    printf("Ingrese un valor: ");
+    scanf("%d", &vec[i]);
+    }
 }
